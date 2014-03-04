53a39e0de85bc93335a9e7500ba9713e5034358a
Lexx <hi@lexx.gr>
2014-03-04 06:31:47 +0200

    Jqueryfied the code
    ...and added some new functionality

--- current.js
+++ current.js
@@ -6,44 +6,89 @@
  * Released under the MIT license
  */
 
-function Currentjs(selector, parentEl, classname) {
-    var self = this;
-    self.selector    = typeof selector  !== 'undefined' ? selector  : 'a';   
-    self.parentEl    = typeof parentEl  !== 'undefined' ? parentEl  : false;   
-    self.classname   = typeof classname !== 'undefined' ? classname : 'active';   
-    self.filter();
-}
+ ;(function ( $, window, document, undefined ) {
 
-Currentjs.prototype.filter = function(){
-    var self = this;
-    var absoluteLink = window.location.href;
-    var relativeLink = window.location.pathname;
-    absoluteLink = self.trimSlash(absoluteLink);
-    relativeLink = self.trimSlash(relativeLink);
-    $(self.selector).each(function(){
+    // Create the defaults once
+    var currentjs = 'currentjs',
+        defaults = {
+            selector: "a",
+            parentElement: false,
+            classname: 'active',
+            startsonly: false
+        };
 
-        var menulink = $(this).attr('href');
-        menulink = self.trimSlash(menulink);
+    // The actual plugin constructor
+    function Plugin( element, options ) {
+        this.element = element;
 
-        if ( absoluteLink == menulink 
-             || relativeLink == menulink 
-        ) {
-            if (self.parentEl !== false) {
-                $(this).closest(self.parentEl).addClass(self.classname);
+        // jQuery has an extend method that merges the
+        // contents of two or more objects, storing the
+        // result in the first object. The first object
+        // is generally empty because we don't want to alter
+        // the default options for future instances of the plugin
+        this.options = $.extend( {}, defaults, options) ;
+
+        this._defaults = defaults;
+        this._name = currentjs;
+
+        this.init();
+    }
+
+    Plugin.prototype.init = function () {
+        this.filter();
+    };
+
+    // A really lightweight plugin wrapper around the constructor,
+    // preventing against multiple instantiations
+    $.fn[currentjs] = function ( options ) {
+        return this.each(function () {
+            if (!$.data(this, 'plugin_' + currentjs)) {
+                $.data(this, 'plugin_' + currentjs,
+                new Plugin( this, options ));
             }
-            $(this).addClass(self.classname);
-        }
-        
-    });
-};
+        });
+    }
 
-Currentjs.prototype.trimSlash = function(str) {
-    str = str.trim();
-    if(str.substr(-1) == '/') {
-        str = str.substr(0, str.length - 1);
+    Plugin.prototype.filter = function(){
+        var self = this;
+        var absoluteLink = window.location.href;
+        var relativeLink = window.location.pathname;
+        absoluteLink = self.trimSlash(absoluteLink);
+        relativeLink = self.trimSlash(relativeLink);
+        var isActive = false;
+        $(self.options.selector).each(function(){
+
+            var menulink = $(this).attr('href');
+            menulink = self.trimSlash(menulink);
+
+            if (self.options.startsonly === true) {
+                if (absoluteLink.indexOf(menulink) != -1 || relativeLink.indexOf(menulink) != -1) {
+                    isActive = true;
+                }
+            }else if ( absoluteLink == menulink || relativeLink == menulink ) {
+                isActive = true;
+            }
+            if (isActive) {
+                if (self.options.parentElement !== false) {
+                    $(this).closest(self.options.parentElement).addClass(self.options.classname);
+                }
+                $(this).addClass(self.options.classname);
+            }
+            isActive = false;
+        });
+    };
+
+    Plugin.prototype.trimSlash = function(str) {
+        str = str.trim();
+        if(str.substr(-1) == '/') {
+            str = str.substr(0, str.length - 1);
+        }
+        if(str.substr(0,1) == '/') {
+            str = str.substr(1);
+        }
+        return str;
     }
-    if(str.substr(0,1) == '/') {
-        str = str.substr(1);
-    }
-    return str;
-}
+
+})( jQuery, window, document );
+
+
